f51e3b29fb707d161573bdd4c8ae320d539e40be
koyli <david@lecomber.net>
2022-03-01 01:01:08 +0000

    ?

--- include/esp32-aws-s3.h
+++ include/esp32-aws-s3.h
@@ -1,10 +1,9 @@
 #include <SD.h>
 
-extern "C++" {
-    class AWS_S3 {
-    public:
-        static int put(String path, File payload);
-        static int put(String path, const byte payload[], int length);
-    };
+
+class AWS_S3 {
+ public:
+    static int put(String path, File payload);
+    static int put(String path, const byte payload[], int length);
+};
     
-}
