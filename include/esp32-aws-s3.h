b6c3395e4af8fa8fb7b41ab0c658f882aa478fc9
koyli <david@lecomber.net>
2022-03-01 00:39:22 +0000

    headers fixing declns

--- include/esp32-aws-s3.h
+++ include/esp32-aws-s3.h
@@ -1,5 +1,5 @@
 #include <SD.h>
 
-int put(String path, File payload);
-int put(String path, const byte payload[], int length);
+extern int put(String path, File payload);
+extern int put(String path, const byte payload[], int length);
 
